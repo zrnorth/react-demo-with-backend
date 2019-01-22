39659f7c3f8b3ae80981ec68a727c361cb60d4ac
Zach North <zrnorth@gmail.com>
2019-01-22 13:04:53 -0800

    Bcrypted passwords + jwt

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,8 +1,22 @@
+const jwt = require("jwt-simple");
 const User = require("../models/user");
+const config = require("../config"); // This doesn't exist in git, remember to create it
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+  // note: sub = subject, iat = issued at time
+}
 
 exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
+
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: "You must provide an email and password" });
+  }
 
   // See if a user with the given email exists
   User.findOne({ email: email }, function(err, existingUser) {
@@ -22,7 +36,7 @@ exports.signup = function(req, res, next) {
       }
 
       // response to request indicating created or not
-      res.json(user);
+      res.json({ token: tokenForUser(user) });
     });
   });
 };
