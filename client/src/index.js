e0a04c50fd5cf1a015313e6f5ec861f795416004
Zach North <zrnorth@gmail.com>
2019-01-22 14:04:44 -0800

    added frontend with some basic scaffolding

---
+++ client/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import App from "./components/App";
+import Welcome from "./components/Welcome";
+import Signup from "./components/auth/Signup";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App>
+      <Route path="/" exact component={Welcome} />
+      <Route path="/signup" exact component={Signup} />
+    </App>
+  </BrowserRouter>,
+  document.querySelector("#root")
+);
